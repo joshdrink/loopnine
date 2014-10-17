d5adc77cf4326e446b13f0220901ebae1f55959e
CodeFusionOne <codefusion1@gmail.com>
2014-10-17 18:27:36 -0300

    Started object logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // Nocturne Dance Grid / 2014 --------------------------------------------------
 
 	// Authored by: Josh Beveridge @ Norex
+    // Modified by: Kyle Ramey @ Norex
 	// 2014/10
 
 // Square ----------------------------------------------------------------------
@@ -16,5 +17,38 @@
 		var boxWidth = $('.art').width();
 
 		$('.row').css('height', boxWidth + "px");
+
+
+        //Web Sockets
+        var webSocket = new WebSocket("ws://echo.websocket.org");
+
+        webSocket.onmessage = function(event){
+            $('#result').append(event.data + '\n');
+
+            var res = parseInt(event.data);
+
+            if(res > 0 && res < 10){
+                $('#' + res).removeClass('secondary').addClass('success');
+            }else if(res < 0 && res > -10){
+                $('#' + (res*-1)).removeClass('success').addClass('secondary');
+            }
+        }
+
+        $('.art').on('click', function(e){
+            parent = $(e.target).parent('.art');
+           if(parent.hasClass('active'))
+                deactivate(parent.attr('id'));
+            else
+                activate(parent.attr('id'));
+        });
+
+        function activate(num){
+            $('#' + num).addClass('active');
+        }
+
+        function deactivate(num) {
+            abs = Math.abs(num);
+            $('#' + abs).removeClass('active');
+        }
 
 	});
