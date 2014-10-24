48ef9a159e449e3697da0ce3338a064bd343526f
Josh Beveridge <joshua.beveridge@icloud.com>
2014-10-24 12:25:05 -0300

    Incredible Fix

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -2,55 +2,55 @@ loopList = {
     1: {
         text: 'In-Flight Safety',
         song: 'Stockholm',
-        image: 'In-Flight Safety.jpg',
+        image: 'in_flight_safety.jpg',
         url: 'in_flight_safety-stockholm-guitar.wav'
     },
     2: {
         text: 'Heather Green',
         song: 'Day I Was Born',
-        image: 'Heather Green.png',
+        image: 'heather_green.png',
         url: 'heather_green-day_i_was_born-rhodes.wav'
     },
     3: {
         text: 'Dylan Guthro',
         song: 'Do It All Again',
-        image: 'Dylan Guthro.jpg',
+        image: 'dylan_guthro.jpg',
         url: 'dylan_guthro-do_it_all_again-guitar.wav'
     },
     4: {
         text: 'AA Wallace',
         song: 'VLT Girls',
-        image: 'AAWallace3.jpeg',
+        image: 'aa_wallace.jpeg',
         url: 'aa_wallace-vlt_girls-synth.wav'
     },
     5: {
         text: 'Quake Matthews',
         song: 'Summertime Win',
-        image: 'Quake Matthews.jpg',
+        image: 'quake_matthews.jpg',
         url: 'quake_matthews-summertime_win-synth.wav'
     },
     6: {
         text: 'Mary Beth de Scène',
         song: 'Henri',
-        image: 'Mary Beth de Scène.JPG',
+        image: 'mary_beth_de_scene.JPG',
         url: 'mary_beth_de_scene-henri-accordion.wav'
     },
     7: {
         text: 'Rich Aucoin',
         song: 'Want To Believe',
-        image: 'Rich Aucoin.jpg',
+        image: 'rich_aucoin.jpg',
         url: 'rich_aucoin-want_to_believe_drums_3.wav'
     },
     8: {
         text: 'Alert The Medic',
         song: 'Grace Come Back',
-        image: 'Alert The Medic.jpg',
+        image: 'alert_the_medic.jpg',
         url: 'alert_the_medic-grace_come_back-drums.wav'
     },
     9: {
         text: 'Glory Glory',
         song: 'Indigo Son',
-        image: 'Glory Glory.jpg',
+        image: 'glory_glory.jpg',
         url: 'glory_glory-indigo_son-drums.wav'
     }
 }
